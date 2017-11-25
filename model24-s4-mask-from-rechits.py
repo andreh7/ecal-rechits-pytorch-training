bugfix: using a (multi-)sigmoid layer now at the output instead of a softmax to allow for more than one output to be close to one

model24-s4-mask-from-rechits.py
@@ -83,9 +83,15 @@ class Model(nn.Module):
 
             nn.Linear(in_features = hidden_size, out_features = input_size), ReLU(),
 
-            # we want outputs in the range 0..1 and
-            # they should sum to one
-            nn.Softmax(),
+            # in principle we want outputs in the range 0..1 and
+            # they should sum to four (not to one !)
+            # 
+            # but for the moment we use (independent) sigmoids and therefore
+            # can have arbitrary shapes
+            # 
+            # (we could also put a constraint that there is a (2x2) window
+            # but let's try not to impose this)
+            nn.Sigmoid(),
 
             # in principle we don't need to reshape
             # back to the original form because